1dce3f4c97716a7311a93eb9c6e0b9345eb3a6b1
Rathi <github email address>
2017-05-29 23:24:07 -0400

    understood event handler

--- js/events.js
+++ js/events.js
@@ -1,7 +1,27 @@
-//define functions here
+function getIt() {
+  $('p').on('click',function() {
+    alert("Hey!")
+  })
+}
+
+function frameIt(){
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt(){
+  $('#typing').on('keydown', function(key){
+    if(key.which == 71){
+      alert('G is been pressed!')
+    }
+  })
+}
+function submitIt() {
+  $("form").on("submit", function() {
+  alert ("Your form is going to be submitted now.")
+})
+}
 
 $(document).ready(function(){
-
-// call functions here
-
 });
